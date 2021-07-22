faa48c1a834ae9142ee3374ba57653a66868ad23
Ǥнσsτ♛ <86915159+arjunsangu@users.noreply.github.com>
2021-07-22 22:47:56 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>🌟 ADMIN ONLY COMMANDS 🌟 \n\n /start - start the bot or get posts \n\n /batch - create link for more than one posts \n\n /genlink - create link for one post \n                  •━━━ ✽ • ✽ ━━━• \n\n○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</b></a>",
+            text = f"<b>🌟 ADMIN ONLY COMMANDS 🌟 \n\n /start - start the bot or get posts \n\n /batch - create link for more than one posts \n\n /genlink - create link for one post \n                  •━━━ ✽ • ✽ ━━━• \n\n○ CREATOR : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ LANGUAGE : <code>Python3</code>\n○ LIBRARY : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ SOURCE CODE : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here \n ○THANKS TO :CODEXBOTZ</b></a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
